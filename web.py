19675842e7fbeb60c69bb047a4bf5971f07986b7
Chris <christineblack@Christines-MacBook-Air-2.local>
2016-10-05 16:46:31 -0400

    Created an about page and added Bootstap

--- web.py
+++ web.py
@@ -1,8 +1,12 @@
-from flask import Flask 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/') # / is the homepage route of website
 def index():
-	return "Hey world!"
+	return render_template('index.html')
+
+@app.route('/about') # This is for different page "About"
+def about:
+	return render_template('about.html')
 
 app.run(debug=True)
